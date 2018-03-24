58bc6b234df9d766e12edc8bf0ae86d9ba21bd73
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-24 18:30:15 +0100

    add Part 1

--- block.go
+++ block.go
@@ -1,12 +1,34 @@
 package main 
 
 import (
-"fmt"
-
+    "bytes"
+    "crypto/sha256"
+    "strconv"
+    "time"
 )
 
-func Print(){
-    
-    
-    fmt.Println()
+type Block struct{
+    Timestamp int64
+    Data byte[]
+    PreBlockHash byte[]
+    Hash byte[]
 }
+
+func NewBlock(data string, preBlockHash []byte) *Block{
+    block := &Block{time.Now().Unix(), []byte(data),
+        preBlockHash, []byte{}}
+    block.SetHash()
+    return block
+}
+
+func(b *Block) SetHash(){
+    timestamp := []byte{strcov.FormatInt(b.Timestamp,10)}
+    headers := bytes.Join([][]byte{b.PreBlockhash, b.Data, timestamp}, []byte{})
+    hash := sha256.Sum256(headers)
+    b.Hash= hash[:]
+}
+
+func NewGenesisBlock() *Block{
+    return NewBlock("Genesis Block",[]byte{})
+}
+
--- main.go
+++ main.go
@@ -5,8 +5,15 @@ import (
 
 )
 
-func Print(){
-    
-    
-    fmt.Println()
+func main(){
+    bc := NewBlockchain()
+    bc.AddBlock("Send 1 BTC to Peng")
+    bc.AddBlock("Send 2 BTC to Peng")
+
+    for _,block := range bc.blocks{
+        fmt.Printf("Prev. Hash: %x\n",block.PrevBlockHash)
+        fmt.Printf("Data: %s\n",block.Data)
+        fmt.Printf("Hash: %s\n",block.Hash)
+        fmt.Println()
+    }
 }
