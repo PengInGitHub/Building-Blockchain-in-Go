74620f3210266f3151cb1500bab422788aace0c5
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-26 20:48:00 +0200

    add addBlock() and printChain()

--- cli.go
+++ cli.go
@@ -10,6 +10,42 @@ type CLI struct{
     bc *Blockchain
 }
 
+func(cli *CLI) printUsage(){
+    fmt.Println("Usage:")
+    fmt.Println(" addBlock -data BLOCK_DATA - add a block to the blockchain")
+    fmt.Println(" printChain - print all of blocks of the chain")
+}
+
+func(cli *CLI) validateArgs(){
+    if len(os.Args)<2{
+        cli.printUsage()
+        os.Exit(1)
+    }
+}
+
+func(cli *CLI) addBlock(data string){
+    cli.bc.AddBlock(data)
+    fmt.Println("success!")
+}
+
+func(cli *CLI) printChain(){
+    bci := cli.bc.Iterator()
+
+    for {
+        block := bci.Next()
+        fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+        fmt.Printf("Data: %s\n", block.Data)
+        fmt.Printf("Hash: %x\n", block.Hash)
+        pow := NewProofOfWork(block)
+        fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+        fmt.Println()
+
+        if len(block.PrevBlockHash) == 0 {
+            break
+        }
+    }
+}
+
 //Run() parses command line arguments and process commands
 func(cli *CLI) Run(){
     cli.validateArgs()
